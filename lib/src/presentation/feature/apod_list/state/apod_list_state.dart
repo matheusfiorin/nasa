96a19a8b10b900468d277eef76c607cee434f9d1
Matheus Fiorin <matheus.fiorin@nubank.com.br>
2024-12-26 16:11:54 -0300

    Define ApodListState using freezed

--- lib/src/presentation/feature/apod_list/state/apod_list_state.dart
+++ lib/src/presentation/feature/apod_list/state/apod_list_state.dart
@@ -1,41 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:nasa/src/domain/entity/apod.dart';
 
-class ApodListState {
-  final List<Apod> apods;
-  final bool isLoading;
-  final bool isLoadingMore;
-  final String error;
-  final String searchQuery;
-  final bool hasReachedEnd;
-  final DateTime? oldestLoadedDate;
+part 'apod_list_state.freezed.dart';
 
-  const ApodListState({
-    this.apods = const [],
-    this.isLoading = false,
-    this.isLoadingMore = false,
-    this.error = '',
-    this.searchQuery = '',
-    this.hasReachedEnd = false,
-    this.oldestLoadedDate,
-  });
-
-  ApodListState copyWith({
-    List<Apod>? apods,
-    bool? isLoading,
-    bool? isLoadingMore,
-    String? error,
-    String? searchQuery,
-    bool? hasReachedEnd,
+@freezed
+class ApodListState with _$ApodListState {
+  const factory ApodListState({
+    @Default([]) List<Apod> apods,
+    @Default(false) bool isLoading,
+    @Default(false) bool isLoadingMore,
+    @Default(false) bool hasReachedEnd,
+    @Default('') String error,
+    @Default('') String searchQuery,
     DateTime? oldestLoadedDate,
-  }) {
-    return ApodListState(
-      apods: apods ?? this.apods,
-      isLoading: isLoading ?? this.isLoading,
-      isLoadingMore: isLoadingMore ?? this.isLoadingMore,
-      error: error ?? this.error,
-      searchQuery: searchQuery ?? this.searchQuery,
-      hasReachedEnd: hasReachedEnd ?? this.hasReachedEnd,
-      oldestLoadedDate: oldestLoadedDate ?? this.oldestLoadedDate,
-    );
-  }
+  }) = _ApodListState;
 }
